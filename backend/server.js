9cf6f8cfb49fe0db47d9049d6e977cc44afaf5c0
Mukesh Swain <mukeshswain2844@gmail.com>
2025-03-07 11:42:29 +0530

    nb

--- backend/server.js
+++ backend/server.js
@@ -11,24 +11,21 @@ import incomeRoute from "./route/income.route.js";
 import expenseRoute from "./route/expense.route.js";
 import dashboardRoute from "./route/dashboard.route.js";
 
-const port = process.env.PORT || 5001;
+// Connect to Database (Only once)
+connectDB();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-
 app.get("/", (req, res) => {
-  res.send("hello world");
-})
+  res.send("Hello world from Vercel!");
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/income", incomeRoute);
 app.use("/api/expense", expenseRoute);
 app.use("/api/dashboard", dashboardRoute);
-app.listen(port, () => {
-  connectDB();
-  console.log(`server is running on http://localhost:${port}`);
-});
 
-export default app
+export default app; // No app.listen()
